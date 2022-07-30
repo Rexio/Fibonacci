7a86b62128cb974f1beae608a4a24550c049a669
Rexio <89345576+Rexio@users.noreply.github.com>
2022-07-30 20:57:39 +0300

    Uusi vedos - jee, toimii, kun sulki ja avas tiedoston

--- 11235byrecursion_2022-07-30-la_v2.py
+++ 11235byrecursion_2022-07-30-la_v2.py
@@ -1,13 +1,15 @@
 "Calculating the nth member of 11235 by recursion"
 def recurf(nth):
+    result = 1
     if nth >2:
         result = recurf(nth-2) + recurf(nth-1)
-        print(result)
+        #print(result)
         #list rvector[]
         #rvector[nth] = result
     else:
         result = 1
         #rvector = 1
+    print(result)
     return result
     #return rvector
 print(recurf(5))
