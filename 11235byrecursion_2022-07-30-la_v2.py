874554d5fb11d77d35d52e763519c5de471326c2
Rexio <89345576+Rexio@users.noreply.github.com>
2022-07-30 20:47:51 +0300

    Uutta yritystä

---
+++ 11235byrecursion_2022-07-30-la_v2.py
@@ -0,0 +1,16 @@
+"Calculating the nth member of 11235 by recursion"
+def recurf(nth):
+    if nth >2:
+        result = recurf(nth-2) + recurf(nth-1)
+        print(result)
+        #list rvector[]
+        #rvector[nth] = result
+    else:
+        result = 1
+        #rvector = 1
+    return result
+    #return rvector
+print(recurf(5))
+
+print("\n\nOnko tää tämä ? Recursion Example Results")
+recurf(5)
